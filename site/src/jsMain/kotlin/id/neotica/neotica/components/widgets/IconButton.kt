c1dd08a311f0f5e5c9dc3427cd57d19ca4586120
laetuz <laetuzg@gmail.com>
2025-06-13 02:02:58 +0700

    Initial commit

---
+++ site/src/jsMain/kotlin/id/neotica/neotica/components/widgets/IconButton.kt
@@ -0,0 +1,21 @@
+package id.neotica.neotica.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.setVariable
+import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.forms.ButtonVars
+import org.jetbrains.compose.web.css.em
+import id.neotica.neotica.CircleButtonVariant
+import id.neotica.neotica.UncoloredButtonVariant
+
+@Composable
+fun IconButton(onClick: () -> Unit, content: @Composable () -> Unit) {
+    Button(
+        onClick = { onClick() },
+        Modifier.setVariable(ButtonVars.FontSize, 1.em), // Make button icon size relative to parent container font size
+        variant = CircleButtonVariant.then(UncoloredButtonVariant)
+    ) {
+        content()
+    }
+}
