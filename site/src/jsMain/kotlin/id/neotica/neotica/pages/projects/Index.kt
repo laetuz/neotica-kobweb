4aa380a41db520390ceaefb56ac3807a3c29e817
laetuz <laetuzg@gmail.com>
2025-07-01 01:43:40 +0700

    refact: - Projects page revamps.

--- site/src/jsMain/kotlin/id/neotica/neotica/pages/projects/Index.kt
+++ site/src/jsMain/kotlin/id/neotica/neotica/pages/projects/Index.kt
@@ -17,14 +17,14 @@ import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.core.layout.Layout
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.components.text.SpanText
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.style.breakpoint.displayIfAtLeast
-import com.varabyte.kobweb.silk.style.breakpoint.displayUntil
 import id.neotica.neotica.components.NeoColor
 import id.neotica.neotica.components.icons.NeoIcons
 import id.neotica.neotica.components.layouts.NeoLayoutData
+import org.jetbrains.compose.web.css.AlignItems
 import org.jetbrains.compose.web.css.FlexDirection
 import org.jetbrains.compose.web.css.cssRem
 import org.jetbrains.compose.web.css.percent
@@ -59,19 +59,14 @@ fun ProjectsPage() {
             )
         }
 
-        Row (
-            Modifier.displayIfAtLeast(Breakpoint.MD),
-            horizontalArrangement = Arrangement.spacedBy(1.cssRem),
+        SimpleGrid(
+            numColumns = numColumns(1, 3),
+            modifier = Modifier
+                .fillMaxWidth()
+                .gap(1.cssRem)
         ) {
             ProjectRow()
         }
-
-        Column(
-            Modifier.displayUntil(Breakpoint.MD).fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(1.cssRem),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) { ProjectRow() }
-
     }
 }
 
@@ -81,27 +76,13 @@ private fun ProjectRow() {
         name = "Neoverse",
         image = NeoIcons.NEOVERSE,
         desc = "An RPG inspired social media.",
-        techStacks = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                val neoverseStacks = listOf(
-                    NeoIcons.KOTLIN,
-                    NeoIcons.ANDROID,
-                    NeoIcons.COMPOSE,
-                    NeoIcons.KOIN,
-                    NeoIcons.FIRESTORE,
-                )
-
-                neoverseStacks.forEach {
-                    Image(
-                        src = it,
-                        width = 30,
-                        modifier = Modifier
-                    )
-                }
-            }
-        }
+        techStacks = listOf(
+            NeoIcons.KOTLIN,
+            NeoIcons.ANDROID,
+            NeoIcons.COMPOSE,
+            NeoIcons.KOIN,
+            NeoIcons.FIRESTORE,
+        )
     ) {
         Link("https://play.google.com/store/apps/details?id=id.neotica.neoverse") {
             SpanText("Play store")
@@ -112,45 +93,18 @@ private fun ProjectRow() {
         name = "Alexandria",
         image = NeoIcons.ALEXANDRIA,
         desc = "A vast community-driven reading and writing platform.",
-        techStacks = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                val alexandriaStacks = listOf(
-                    NeoIcons.KOTLIN,
-                    NeoIcons.ANDROID,
-                    NeoIcons.APPLE,
-                    NeoIcons.CMP,
-                    NeoIcons.KOIN,
-                )
-
-                alexandriaStacks.forEach {
-                    Image(
-                        src = it,
-                        width = 30,
-                        modifier = Modifier
-                    )
-                }
-            }
-
-            Row {
-                val alexandriaStacks2 = listOf(
-                    NeoIcons.NGINX,
-                    NeoIcons.KTOR,
-                    NeoIcons.POSTGRESQL,
-                    NeoIcons.DOCKER,
-                    NeoIcons.EC2,
-                )
-
-                alexandriaStacks2.forEach {
-                    Image(
-                        src = it,
-                        width = 30,
-                        modifier = Modifier
-                    )
-                }
-            }
-        }
+        techStacks = listOf(
+            NeoIcons.KOTLIN,
+            NeoIcons.ANDROID,
+            NeoIcons.APPLE,
+            NeoIcons.CMP,
+            NeoIcons.KOIN,
+            NeoIcons.NGINX,
+            NeoIcons.KTOR,
+            NeoIcons.POSTGRESQL,
+            NeoIcons.DOCKER,
+            NeoIcons.EC2,
+            )
     ) {
         Link("https://alexandria.neotica.id") {
             SpanText("Website")
@@ -162,23 +116,11 @@ private fun ProjectRow() {
         imageSize = 120,
         imageModifier = Modifier.padding(topBottom = 1.cssRem),
         desc = "A Compose Multiplatform library.",
-        techStacks = {
-            Row {
-                val alexandriaStacks = listOf(
-                    NeoIcons.KOTLIN,
-                    NeoIcons.MAVENCENTRAL,
-                    NeoIcons.CMP,
-                )
-
-                alexandriaStacks.forEach {
-                    Image(
-                        src = it,
-                        width = 30,
-                        modifier = Modifier
-                    )
-                }
-            }
-        }
+        techStacks = listOf(
+            NeoIcons.KOTLIN,
+            NeoIcons.MAVENCENTRAL,
+            NeoIcons.CMP,
+        )
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -200,22 +142,10 @@ private fun ProjectRow() {
         imageSize = 120,
         imageModifier = Modifier.padding(topBottom = 1.cssRem),
         desc = "KotSpam is a Kotlin-based tool designed to simulate automated message for spamming.",
-        techStacks = {
-            Row {
-                val alexandriaStacks = listOf(
-                    NeoIcons.KOTLIN,
-                    NeoIcons.JAVA
-                )
-
-                alexandriaStacks.forEach {
-                    Image(
-                        src = it,
-                        width = 30,
-                        modifier = Modifier
-                    )
-                }
-            }
-        }
+        techStacks = listOf(
+            NeoIcons.KOTLIN,
+            NeoIcons.JAVA
+        )
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -236,7 +166,7 @@ private fun ProjectCard(
     imageSize: Int? = 180,
     imageModifier: Modifier? = Modifier,
     desc: String,
-    techStacks: @Composable () -> Unit,
+    techStacks: List<String>,
     content: @Composable () -> Unit
 ) {
     Column(
@@ -245,7 +175,7 @@ private fun ProjectCard(
             .backgroundColor(NeoColor.colorPrimary20)
             .fillMaxSize()
             .padding(2.cssRem)
-            .width(16.cssRem)
+//            .width(16.cssRem)
             .flexDirection(FlexDirection.Column),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -276,6 +206,17 @@ private fun ProjectCard(
                 .padding(top = 1.cssRem)
                 .fontSize(0.9.cssRem)
         )
-        techStacks()
+        SimpleGrid(
+            numColumns = numColumns(5, 5),
+            modifier = Modifier.alignItems(AlignItems.Center)
+        ) {
+            techStacks.forEach {
+                Image(
+                    src = it,
+                    width = 30,
+                    modifier = Modifier
+                )
+            }
+        }
     }
 }
